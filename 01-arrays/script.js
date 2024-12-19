6702f5f0c52e217888b32a823408ceeb92180460
Alex Mihov <alexmihov.as@gmail.com>
2024-12-19 13:11:06 +0100

    feat dall 2 al 8

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -13,21 +13,31 @@ const teachers = [
 const fourthTeacher = null;
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+let removedQuinto = teachers.toSpliced(teachers.indexOf('Carlo'), 1, 'Patrick');
+console.log(removedQuinto);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();
+console.log(teachers, lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
+console.log(teachers, firstTeacher);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+let addVanessa = teachers.push('Vanessa');
+console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+let addSarah = teachers.unshift('Sarah');
+console.log(teachers);
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const lewisIndex = teachers.indexOf('Lewis');
+console.log(lewisIndex)
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = !teachers.length;
+console.log(isTeachersEmpty)
